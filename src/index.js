50a30cdc69c081cf254576c65d0e8bb35b2ffbc3
Sswingle <seiji.swingl@gmail.com>
2019-01-24 13:30:37 -0800

    updated component lifecycle

--- src/index.js
+++ src/index.js
@@ -4,18 +4,32 @@ import ReactDOM from 'react-dom';
 class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = { lat:null };
+    this.state = {
+      lat:null,
+      errorMessage: ''
+    };
+  }
+componentDidMount(){
+  window.navigator.geolocation.getCurrentPosition(
+    position => {
+      this.setState({ lat: position.coords.latitude });
+    },
+    err =>  {this.setState({ errorMessage: err.message})}
+  );
+}
+// componentDidUpdate(){
+//   console.log('component has been updated')
+// }
 
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => console.log(err)
-    );
+render(){
+  if(this.state.errMessage && !this.state.lat){
+    return <div>Error: {this.state.errorMessage}</div>
   }
-  render(){
-    return <div>Latitude: {this.state.lat}</div>
+  if(!this.state.errMessage && this.state.lat){
+    return <div> Latitude: {this.state.lat}</div>
   }
+  return <div> Loading! </div>
+  };
 }
 
 ReactDOM.render(
