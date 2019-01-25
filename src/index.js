70c4716b4e4750a3f469862c2ff8061ad7965497
Sswingle <seiji.swingl@gmail.com>
2019-01-24 18:27:58 -0800

    separated render content into helper function

--- src/index.js
+++ src/index.js
@@ -18,22 +18,32 @@ componentDidMount(){
     position => {
       this.setState({ lat: position.coords.latitude });
     },
-    err =>  {this.setState({ errorMessage: err.message})}
+    err =>  this.setState({ errorMessage: err.message})
   );
 }
 // componentDidUpdate(){
 //   console.log('component has been updated')
 // }
+  renderContent() {
+    if(this.state.errMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+    }
 
-render(){
-  if(this.state.errMessage && !this.state.lat){
-    return <div>Error: {this.state.errorMessage}</div>
-  }
-  if(!this.state.errMessage && this.state.lat){
-    return <SeasonDisplay lat={this.state.lat} />
-  }
-  return <Spinner message='Please accept location request'/>
+    if(!this.state.errMessage && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <Spinner message='Please accept location request'/>
+
   };
+
+  render(){
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
