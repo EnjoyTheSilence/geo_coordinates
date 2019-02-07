104b76b1d7a0b423ee47f3492e9eb65c3f39d300
Sswingle <seiji.swingl@gmail.com>
2019-02-07 12:20:51 -0800

    refactored to use React hooks instead of class component

--- src/index.js
+++ src/index.js
@@ -2,48 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import userLocation from './userLocation';
 
-class App extends React.Component{
-  //state = { lat: null, errorMessage: ''};
-    //equivalent to creating constructor and setting this.state
-  constructor(props){
-    super(props);
-    this.state = {
-      lat:null,
-      errorMessage: ''
-    };
-  }
-componentDidMount(){
-  window.navigator.geolocation.getCurrentPosition(
-    position => {
-      this.setState({ lat: position.coords.latitude });
-    },
-    err =>  this.setState({ errorMessage: err.message})
-  );
-}
-// componentDidUpdate(){
-//   console.log('component has been updated')
-// }
-  renderContent() {
-    if(this.state.errMessage && !this.state.lat){
-      return <div>Error: {this.state.errorMessage}</div>
+const App = () => {
+  const [lat, errorMessage] = userLocation();
+
+  let content;
+    if(errorMessage) {
+      content = <div> Error : {errorMessage}</div>
+    } else if (lat) {
+      content = <SeasonDisplay lat={lat} />
+    } else {
+      content = <Spinner message='Please accept the location request' />
     }
-
-    if(!this.state.errMessage && this.state.lat){
-      return <SeasonDisplay lat={this.state.lat} />
-    }
-
-    return <Spinner message='Please accept location request'/>
-
-  };
-
-  render(){
-    return (
-      <div className="border red">
-        {this.renderContent()}
-      </div>
-    );
-  }
+  return <div className="border red" > {content} </div>
 }
 
 ReactDOM.render(
